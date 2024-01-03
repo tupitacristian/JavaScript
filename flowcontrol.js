4a46f61fc67308d42736929e57ec23d3d5128c62
tupitacristian <56452830+tupitacristian@users.noreply.github.com>
2024-01-03 18:02:49 +0100

    Add files via upload

--- flowcontrol.js
+++ flowcontrol.js
@@ -22,14 +22,25 @@ let number = 1;
 
 // while
 
-while(number <= 10){
-    console.log(number);
-    number++;
-}
+// while(number <= 10){
+//     console.log(number);
+//     number++;
+// }
 
 // do while
 
-do {
-    number++;
-    console.log(number);
-}while(number <= 10);
+// do {
+//     number++;
+//     console.log(number);
+// }while(number <= 10);
+
+switch(number){
+    case 1:
+        console.log("Number este 1");
+        break;
+    case 2:
+        console.log("Number este 2");
+        break;
+    default:
+        console.log("Number este de fapt: " + number);
+}   
